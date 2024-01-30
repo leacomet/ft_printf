71d492dd81419f2e70fbf75858b473110ed6bbd4
Alexandre Comet <acomet@2E3.42angouleme.fr>
2024-01-30 19:12:54 +0100

    save

---
+++ test_ft_printf.c
@@ -0,0 +1,21 @@
+#include "ft_printf.c"
+#include <stdio.h>
+#include <limits.h>
+
+int	main(void)
+{
+//	int		print_nbr = 0;
+//	int		print_nbr2 = 0;
+//	void	*ptr = "azz";
+
+
+ft_printf("%x", LONG_MAX);
+printf("%x", LONG_MAX);
+
+
+//	print_nbr = ft_printf(  "abc" "def%c%s %d %i %x %X %p %a ", 'Z', "789", -42, +7777777, 777, 2147483647, ptr);
+//	printf("\n%d\n", print_nbr);
+//	print_nbr2 = printf(  "abc" "def%c%s %d %i %x %X %p %%   % ", 'Z', "789", -42, +7777777, 777, 2147483647, ptr);
+//	printf("\n%d\n", print_nbr2);
+	return (0);
+}
